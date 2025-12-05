a05d5a517c6869e5e777cbba9859495c1c2e1d59
mezenbayounes <61406514+mezenbayounes@users.noreply.github.com>
2025-12-05 17:40:13 +0100

    update order id

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,13 @@ import hanaLogo from '../assets/hanaModeLogo.png';
 export default function Footer() {
   const { t } = useTranslation();
   return (
+  
+    
+    
     <footer className="bg-white text-gray-900 font-hana">
+
       <div className="max-w-7xl mx-auto px-4 py-12">
+        
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8 justify-items-start md:justify-items-center w-full">
           
           {/* Brand */}
@@ -125,5 +130,6 @@ export default function Footer() {
         </div>
       </div>
     </footer>
+   
   );
 }
